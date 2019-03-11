3162bd30ba553962435da8a322a23f97b3149a6d
vForVengeance <peppemarchese97@gmail.com>
2019-03-11 13:23:47 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InstagramService, Post, Comment } from '../instagram.service';
+import { ToastController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  public posts: Post[] = [];
+  public post: Post; 
+  public author: string;
+  
+  constructor(public instaService: InstagramService, public route: ActivatedRoute, public toastCtrl: ToastController) {
+  }
+
+  ngOnInit() {
+    this.author = this.instaService.getAuthor();
+    
+    this.instaService.all().then(response => {
+      this.posts = response;
+      console.log(this.posts);
+    });
+  }
+  
+  like(id: number) {
+    this.author = this.instaService.getAuthor();
+    this.instaService.addLike(id, this.author).then(() => {
+      this.presentToast('Your like have been sent.');
+      this.loadChatAndMessages();
+    });
+  }
+
+  loadChatAndMessages() {
+    console.log('#loadChatAndMessages');
+    if (this.instaService) {
+      this.instaService.all().then(response => {
+        this.posts = response;
+        console.log(this.posts);
+      });
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { InstagramService, Post } from '../instagram.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  public post: Post = {
+    author: "",
+    image: "",
+    message:""
+  };
+  constructor(public instaService: InstagramService, public toastController: ToastController) {
+  }
+
+  set() {
+    this.instaService.newPost(this.post).then(() => {
+      if(this.instaService.getAuthor()) {
+        this.instaService.getAuthor()
+      }
+      this.presentToast('Post has been created');
+    });
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+}
